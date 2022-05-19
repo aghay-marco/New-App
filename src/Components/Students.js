0f35f86a5d9a4da2de4cb2d8f69ad89e4d85f144
aghay-marco <mr.a.asgari@gmail.com>
2022-05-19 22:21:49 +0430

    end of session 2 lesson 20

--- src/Components/Students.js
+++ src/Components/Students.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import "./Students.css"
-const Students = () => {
+import './Students.css';
+
+const Student = () => {
     return (
         <div className="students">
-            <label>شماره دانش آموز : 1 </label>
-            <label>نام و نام خانوادگی </label>
-            <input type="text" />
+            <label>شماره دانش آموزش : 1 </label>
+            <label>نام و نام خانوادگی</label>
+            <input type='text' />
 
-            <label>کلاس</label>
-            <input type="text" />
+            <label>کلاس </label>
+            <input type='text' />
 
             <label>شماره تلفن</label>
             <input type="number" />
 
-            <label> ایمیل</label>
-            <input type="email" />
+            <label>ایمیل</label>
+            <input type='email' />
         </div>
-    );
+    )
 }
 
-export default Students;
+export default Student;
