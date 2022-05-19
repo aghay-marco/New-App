7a1ae5a202dca9fffe350ab5624963e3f93e3fdd
aghay-marco <mr.a.asgari@gmail.com>
2022-05-19 19:05:01 +0430

    delet all component test and  begin new project (Student project)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import Greeting from "./Components/Greeting";
-import Website from './Components/Welcome'
-const App = () => {
-  // const clickHandler = (props) => {
-  //   alert("hi" + " " + props)
-  // }
-  
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', fontSize: 22 }} >
 
-      {/* <Website age={25} handler={() => clickHandler("Mahdi")} />
-      <Website name="amir" age={24} handler={() => clickHandler("amir")} /> */}
-      <Greeting isLoggedIn={true} />
+const App = () => {
+  return (
+    <div >
+
     </div >
   )
 }
