98f7dcf4b8afbc718f1646a9966e54cc38def985
aghay-marco <mr.a.asgari@gmail.com>
2022-06-11 00:40:53 +0430

    end of session 26

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Students from "./Components/Students";
 import './App.css'
-
+import Button from "./Components/UI/button/button";
 const App = () => {
   const [studentsState, setStudents] = useState([
     { id: 0, name: "mahdi", classNumber: 204, phoneNumber: 123, email: "mr.a.asgari@gmail.com" },
     { id: 1, name: "saeed", classNumber: 205, phoneNumber: 123, email: "mr.a.asgari@gmail.com" },
     { id: 2, name: "amir", classNumber: 206, phoneNumber: 123, email: "mr.a.asgari@gmail.com" },
     { id: 3, name: "amirali", classNumber: 207, phoneNumber: 123, email: "mr.a.asgari@gmail.com" },
-  ])
+  ]);
+  const [arrayHolder, setArrayHolder] = useState([]);
+  const [toggle, setToggle] = useState(false)
+  const [searchBarValue, setSearchBarValue] = useState('');
+  useEffect(() => {
+    setArrayHolder(studentsState)
+  }, []);
+  const searchFilterFunction = (event) => {
+    const itemData = arrayHolder.filter((item) => {
+      const itemsData = item.name.toUpperCase();
+      const textData = event.target.value.toUpperCase();
+      return itemsData.indexOf(textData) > -1
+    })
+    setStudents(itemData);
+    setSearchBarValue(event.target.value);
+  }
+
   const nameChangeHandler = (event, id) => {
     const studentIndex = studentsState.findIndex(s => {
       return s.id === id
@@ -58,8 +74,21 @@ const App = () => {
     students.splice(index, 1);
     setStudents(students)
   }
+
+  const toggleHandler = () => {
+    setToggle(!toggle)
+    console.log(toggle)
+  }
   return (
     <div className="App">
+      <input type="text" value={searchBarValue} onChange={searchFilterFunction} />
+      <Button
+        btnType="success"
+        clicked={toggleHandler}
+      >
+        تغییر وضعیت نمایش
+      </Button>
+
       <Students
         studentsList={studentsState}
         nameChanged={nameChangeHandler}
@@ -67,6 +96,7 @@ const App = () => {
         phoneNumberChanged={phoneNumberChangeHandler}
         emailChanged={emailChangeHandler}
         deleted={deleteStudent}
+        toggle={toggle}
       />
     </div >
   )
--- src/Components/Students.js
+++ src/Components/Students.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import Student from './student/student';
-
+import './student/Students.css'
 const Students = (props) => {
+    if (props.toggle) {
+        return (
+            <div className='student-section'>
+                {
+                    props.studentsList.map((person, index) =>
+                        <Student
+                            key={index}
+                            id={person.id}
+                            name={person.name}
+                            classNumber={person.classNumber}
+                            phoneNumber={person.phoneNumber}
+                            email={person.email}
+                            nameChanged={(event) => props.nameChanged(event, person.id)}
+                            classNumberChanged={(event) => props.classNumberChanged(event, person.id)}
+                            phoneNumberChanged={(event) => props.phoneNumberChanged(event, person.id)}
+                            emailChanged={(event) => props.emailChanged(event, person.id)}
+                            deleted={props.deleted}
+                        />
+                    )
+                }
+            </div>
+
+        )
+    }
     return (
         props.studentsList.map((person, index) =>
             <Student
